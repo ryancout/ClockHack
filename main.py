7d8eb39532e8acff29dc33d5888e9492b3baa213
ryancout <ryancout@icloud.com>
2026-05-11 11:33:30 -0300

    Release v6.3.1 - build automatizado

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 from app.ui.main_window import iniciar_app
 
 if __name__ == "__main__":
